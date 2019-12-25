9bef7d559ec9c939790ffa89baf3dc1002fc9145
kzyanisara <kzyanisara@yahoo.com>
2019-12-25 19:27:02 +0700

    เพิ่มการตรวจสอบของ บัตร ปชช

--- app_check_pid.py
+++ app_check_pid.py
@@ -2,7 +2,10 @@ from utils.check_pid import is_valid
 
 if __name__ == '__main__':
     my_pid = '1101401427542'
-    if is_valid(my_pid):
-        print("correct")
+    if len(my_pid) == 13 & my_pid.isnumeric():
+        if is_valid(my_pid):
+            print("correct")
+        else:
+            print("incorrect")
     else:
-        print("incorrect")
+        print("This is not personal ID")
